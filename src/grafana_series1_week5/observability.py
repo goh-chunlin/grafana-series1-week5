af6c4de2f730b5c7862b83a51c704c06b8406fbf
goh-chunlin <chunlin@live.com.sg>
2026-01-04 16:48:04 +0800

    docs: Update comments to clarify W3C trace ID formatting requirements

--- src/grafana_series1_week5/observability.py
+++ src/grafana_series1_week5/observability.py
@@ -1,3 +1,4 @@
+# import secrets # Uncomment if you enforce W3C trace ID formatting
 """
 The O11y Contract: Centralizing our metadata standards.
 If it's not defined here, it doesn't exist in our traces.
@@ -21,6 +22,16 @@ def inject_o11y_headers(trace_id: str) -> dict:
         # However, for the sake of this exercise, we return an empty dict.
         return {}
 
+    # To make this works with the existing tests, we need to ensure
+    # that the trace_id is in W3C format.
+    # You need to uncomment the following lines to enforce W3C formatting.
+    # if not trace_id:
+    #   trace_id = secrets.token_hex(16)
+    # if not trace_id.startswith("00-"):
+    #   span_id = secrets.token_hex(8) 
+    #   trace_id = f"00-{trace_id}-{span_id}-01"
+    # After that, you need to comment out the if not trace_id check above.
+    
     return {
         TRACE_HEADER: trace_id,
         SERVICE_VERSION_HEADER: CURRENT_VERSION
